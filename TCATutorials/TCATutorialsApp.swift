69c779b13a3b9049cb00f4eb7ee54bad759c73d8
Masanori Ogawa <mas.ogawa.app@gmail.com>
2024-11-25 06:29:20 +0900

    Initial Commit

---
+++ TCATutorials/TCATutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATutorialsApp.swift
+//  TCATutorials
+//
+//  Created by Masanori Ogawa on 25/11/R6.
+//
+
+import SwiftUI
+
+@main
+struct TCATutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TCATutorialsTests/TCATutorialsTests.swift
@@ -0,0 +1,17 @@
+//
+//  TCATutorialsTests.swift
+//  TCATutorialsTests
+//
+//  Created by Masanori Ogawa on 25/11/R6.
+//
+
+import Testing
+@testable import TCATutorials
+
+struct TCATutorialsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
