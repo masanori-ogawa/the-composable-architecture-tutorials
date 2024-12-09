42687d3786db4c7423135c17f51a614d63720743
Masanori Ogawa <mas.ogawa.app@gmail.com>
2024-12-09 21:42:55 +0900

    Your first presentation delegateパターン前まで

--- TCATutorials/AppFeature.swift
+++ TCATutorials/AppFeature.swift
@@ -10,47 +10,58 @@ import SwiftUI
 
 @Reducer
 struct AppFeature {
-    struct State: Equatable {
-        var tab1 = CounterFeature.State()
-        var tab2 = CounterFeature.State()
+  struct State: Equatable {
+    var tab1 = ContactsFeature.State()
+    var tab2 = CounterFeature.State()
+    var tab3 = CounterFeature.State()
+  }
+  
+  enum Action {
+    case tab1(ContactsFeature.Action)
+    case tab2(CounterFeature.Action)
+    case tab3(CounterFeature.Action)
+  }
+  
+  var body: some ReducerOf<Self> {
+    // 親Feature（AppFeature）に子Feature（CounterFeature）を組み込むためにScope Reducerを用いる
+    Scope(state: \.tab1, action: \.tab1) {
+      ContactsFeature()
     }
-    
-    enum Action {
-        case tab1(CounterFeature.Action)
-        case tab2(CounterFeature.Action)
+    Scope(state: \.tab2, action: \.tab2) {
+      CounterFeature()
     }
-    
-    var body: some ReducerOf<Self> {
-        // 親Feature（AppFeature）に子Feature（CounterFeature）を組み込むためにScope Reducerを用いる
-        Scope(state: \.tab1, action: \.tab1) {
-            CounterFeature()
-        }
-        Scope(state: \.tab2, action: \.tab2) {
-            CounterFeature()
-        }
-        Reduce { state, action in
-            // Core logic of the app feature
-            return .none
-        }
+    Scope(state: \.tab3, action: \.tab3) {
+      CounterFeature()
     }
+    Reduce { state, action in
+      // Core logic of the app feature
+      return .none
+    }
+  }
 }
 
 struct AppView: View {
-    let store: StoreOf<AppFeature>
-
-    var body: some View {
-        TabView {
-            CounterView(store: store.scope(state: \.tab1, action: \.tab1))
-                .tabItem {
-                    Text("Counter 1")
-                }
-            
-            CounterView(store: store.scope(state: \.tab2, action: \.tab2))
-                .tabItem {
-                    Text("Counter 2")
-                }
+  let store: StoreOf<AppFeature>
+  
+  var body: some View {
+    TabView {
+      ContactsView(store: store.scope(state: \.tab1, action: \.tab1))
+        .tabItem {
+          Text("Contacts")
         }
+
+      CounterView(store: store.scope(state: \.tab2, action: \.tab2))
+        .tabItem {
+          Text("Counter 1")
+        }
+      
+      CounterView(store: store.scope(state: \.tab3, action: \.tab3))
+        .tabItem {
+          Text("Counter 3")
+        }
+
     }
+  }
 }
 
 #Preview {
