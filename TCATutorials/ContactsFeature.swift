5c796095fd7939ddae49bbf5292f5d6b7ef080ea
Masanori Ogawa <mas.ogawa.app@gmail.com>
2024-12-29 13:57:08 +0900

    Multiple presentation destinations

--- TCATutorials/ContactsFeature.swift
+++ TCATutorials/ContactsFeature.swift
@@ -14,37 +14,72 @@ struct Contact: Equatable, Identifiable {
   var name: String
 }
 
+extension ContactsFeature {
+  @Reducer
+  enum Destination {
+    case addContact(AddContactFeature)
+    case alert(AlertState<ContactsFeature.Action.Alert>)
+  }
+}
+
+extension ContactsFeature.Destination.State: Equatable {}
+
 @Reducer
 struct ContactsFeature {
   @ObservableState
   struct State: Equatable {
-    @Presents var addContact: AddContactFeature.State?
     var contacts: IdentifiedArrayOf<Contact> = []
+    @Presents var destination: Destination.State?
   }
+
   enum Action {
     case addButtonTapped
-    case addContact(PresentationAction<AddContactFeature.Action>)
+    case deleteButtonTapped(id: Contact.ID)
+    case destination(PresentationAction<Destination.Action>)
+
+    enum Alert: Equatable {
+      case confirmDeletion(id: Contact.ID)
+    }
   }
+
   var body: some ReducerOf<Self> {
     Reduce { state, action in
       switch action {
       case .addButtonTapped:
-        state.addContact = AddContactFeature.State(
-          contact: Contact(id: UUID(), name: "")
+        state.destination = .addContact(
+          AddContactFeature.State(
+            contact: Contact(id: UUID(), name: "")
+          )
         )
         return .none
-
-      case let .addContact(.presented(.delegate(.saveContact(contact)))):
+        // presentedとは？
+        //  AddContactFeatureのsaveContactアクションをdelegate経由で受け取る。
+        // presentationアクションを受け取る時はpresentedを使う。
+      case let .destination(.presented(.addContact(.delegate(.saveContact(contact))))):
         state.contacts.append(contact)
         return .none
-
-      case .addContact:
+      case let .destination(.presented(.alert(.confirmDeletion(id: id)))):
+        state.contacts.remove(id: id)
+        return .none
+      case .destination:
+        return .none
+      case let .deleteButtonTapped(id):
+        state.destination = .alert(
+          AlertState {
+            TextState("Are you sure?")
+          } actions: {
+            ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
+              TextState("Delete")
+            }
+          }
+        )
         return .none
       }
     }
-    .ifLet(\.$addContact, action: \.addContact) {
-      AddContactFeature()
-    }
+    // なぜifLetが必要？
+    // これは、addContactがnilでない場合にのみ、AddContactFeatureを表示するためです。
+    // これにより、不要なリソースの消費を防ぎます。
+    .ifLet(\.$destination, action: \.destination)
   }
 }
 
@@ -55,7 +90,16 @@ struct ContactsView: View {
     NavigationStack {
       List {
         ForEach(store.contacts) { contact in
-          Text(contact.name)
+          HStack {
+            Text(contact.name)
+            Spacer()
+            Button {
+              store.send(.deleteButtonTapped(id: contact.id))
+            } label: {
+              Image(systemName: "trash")
+                .foregroundStyle(.red)
+            }
+          }
         }
       }
       .navigationTitle("Contacts")
@@ -70,12 +114,13 @@ struct ContactsView: View {
       }
     }
     .sheet(
-      item: $store.scope(state: \.addContact, action: \.addContact)
+      item: $store.scope(state: \.destination?.addContact, action: \.destination.addContact)
     ) { addContactStore in
       NavigationStack {
         AddContactView(store: addContactStore)
       }
     }
+    .alert($store.scope(state: \.destination?.alert, action: \.destination.alert))
   }
 }
 
